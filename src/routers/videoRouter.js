3d8c56617ea1dbfcd6d52afceab5206fdea9a99f
roqurdl <uyhnri@gmail.com>
2022-06-22 22:42:04 +0900

    firstPost

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,16 @@
 import express from "express";
-import { see, edit, deleteVideo, upload } from "../controllers/videoController";
+import {
+  getEdit,
+  postEdit,
+  deleteVideo,
+  upload,
+  watch,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
-videoRouter.get("/:id(\\d+)", see);
-videoRouter.get("/:id(\\d+)/edit", edit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.get("/:id(\\d+)", watch);
+videoRouter.get("/:id(\\d+)/edit", getEdit);
+videoRouter.post("/:id(\\d+)/edit", postEdit);
 
 export default videoRouter;
