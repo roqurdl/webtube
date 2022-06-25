285d6f6bb9b817d155b89083214d6d5a78f21110
roqurdl <uyhnri@gmail.com>
2022-06-25 22:00:38 +0900

    aboutCallback

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,48 +1,29 @@
-let videos = [
-  {
-    title: "First Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 1,
-    id: 1,
-  },
-  {
-    title: "Second Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 55,
-    id: 2,
-  },
-  {
-    title: "Third Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 55,
-    id: 3,
-  },
-];
+import Video from "../models/Video";
 
-export const trending = (req, res) => {
-  return res.render("home", { pageTitle: "Home", videos });
+export const home = (req, res) => {
+  console.log("Start");
+  Video.find({}, (error, videos) => {
+    if (error) {
+      return res.render("Server-error");
+    }
+    return res.render("home", { pageTitle: "Home", videos });
+  });
+  console.log("I should be last");
 };
+
 export const watch = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  res.render(`watch`, { pageTitle: `Watching: ${video.title}`, video });
+  res.render(`watch`, { pageTitle: `Watching` });
 };
 
 export const getEdit = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+
+  res.render("edit", { pageTitle: `Editing` });
 };
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-  videos[id - 1].title = title; // not important just for using fake database
   return res.redirect(`/videos/${id}`);
 };
 
@@ -50,14 +31,6 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: `Uploading Video` });
 };
 export const postUpload = (req, res) => {
-  const newVideo = {
-    title: req.body.title,
-    rating: 0,
-    comments: 0,
-    createdAt: "just now.",
-    views: 0,
-    id: videos.length + 1,
-  };
-  videos.push(newVideo);
+  const { title } = req.body;
   return res.redirect(`/`);
 };
