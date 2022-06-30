ff9078180541f9621b4419778130c7cb5b1159ff
roqurdl <uyhnri@gmail.com>
2022-06-30 22:03:20 +0900

    saveUser

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,32 @@
-export const join = (req, res) => res.send("Join");
-export const edit = (req, res) => res.send("Edit User");
-export const remove = (req, res) => res.send("Delete User");
-export const login = (req, res) => res.send("Login");
-export const logout = (req, res) => res.send("Log out");
-export const see = (req, res) => res.send("See User");
+import User from "../models/User";
+
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "Join" });
+};
+export const postJoin = async (req, res) => {
+  const { name, username, email, password, location } = req.body;
+  await User.create({
+    name,
+    username,
+    email,
+    password,
+    location,
+  });
+  res.redirect("/login");
+};
+
+export const edit = (req, res) => {
+  res.render("Edit User");
+};
+export const remove = (req, res) => {
+  res.render("Delete User");
+};
+export const login = (req, res) => {
+  res.render("login", { pageTitle: "Login" });
+};
+export const logout = (req, res) => {
+  res.render("Log out");
+};
+export const see = (req, res) => {
+  res.render("See User");
+};
