23b1019b1920791c16e1fb47c765f3652fb39c79
roqurdl <uyhnri@gmail.com>
2022-07-11 16:03:37 +0900

    updateDbNotSession

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -7,12 +7,24 @@ import {
   postUpload,
   deleteVideo,
 } from "../controllers/videoController";
+import { protectMiddleware, publicOnlyMiddleware } from "../middlewares";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-f]{24})", watch);
-videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
-videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
-videoRouter.route("/upload").get(getUpload).post(postUpload);
+videoRouter
+  .route("/:id([0-9a-f]{24})/edit")
+  .all(protectMiddleware)
+  .get(getEdit)
+  .post(postEdit);
+videoRouter
+  .route("/:id([0-9a-f]{24})/delete")
+  .all(protectMiddleware)
+  .get(deleteVideo);
+videoRouter
+  .route("/upload")
+  .all(protectMiddleware)
+  .get(getUpload)
+  .post(postUpload);
 
 export default videoRouter;
